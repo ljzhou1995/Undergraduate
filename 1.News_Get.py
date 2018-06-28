b50d2e66a1924b9d5ce287d35e52970b228c8097
ljzhou1995 <15211109354@163.com>
2018-06-28 12:23:44 +0800

    Update 1.News_Get.py

--- 1.News_Get.py
+++ 1.News_Get.py
@@ -1,7 +1,6 @@
 # python3.6
-# 原作者：https://blog.csdn.net/MrWilliamVs/article/details/76422584
-# 修改：刘佳舟
-# 爬取目标：金融界（http://stock.jrj.com.cn）
+# author: ljzhou
+# target: http://stock.jrj.com.cn
 # coding: utf-8
 
 import codecs
@@ -13,42 +12,42 @@ from urllib.error import HTTPError, URLError
 import sys
 
 
-    # 设置递归次数为100万
+    # Set the recursion number to 1 million for deep crawling
 sys.setrecursionlimit(1000000)
 
-    # 伪装浏览器抬头以防和谐
+    # Disguise as a browser to avoid harmony
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
       'Chrome/51.0.2704.63 Safari/537.36'}
 
-    # 新闻类定义
+    # News definition
 class News(object):
     def __init__(self):
-        self.url = None  # 该新闻对应的url
-        self.topic = None  # 新闻标题
-        self.date = None  # 新闻发布日期
-        self.content = None  # 新闻的正文内容
-        self.author = None  # 新闻作者
+        self.url = None  # The url
+        self.topic = None  # The topic/title
+        self.date = None  # The release date
+        self.content = None  # The content/body
+        self.author = None  # The author
 
-#如果url符合解析要求，则对该页面进行信息提取
+## If the url meets the parsing requirements, then extract the page
 def getNews(url):
-    # 获取页面所有元素
+    # Get all the elements of the page
     html = request.urlopen(url).read().decode('gb2312','ignore')
-    # 解析
+    # Extract
     soup = BeautifulSoup(html, 'html.parser')
 
-    # 获取信息
+    # Get information
     if not (soup.find('div', {'id': 'leftnews'})): return
 
-    news = News()  # 建立新闻对象
+    news = News()  # Create news object
 
     page = soup.find('div', {'id': 'leftnews'})
 
     if not (page.find('div', {'class': 'newsConTit'})): return
-    topic = page.find('div', {'class': 'newsConTit'}).get_text()  # 新闻标题
+    topic = page.find('div', {'class': 'newsConTit'}).get_text()  # news topic/title
     news.topic = topic
 
     if not (page.find('div', {'id': 'IDNewsDtail'})): return
-    main_content = page.find('div', {'id': 'IDNewsDtail'})  # 新闻正文内容
+    main_content = page.find('div', {'id': 'IDNewsDtail'})  # news content/body
 
     content = ''
 
@@ -57,30 +56,29 @@ def getNews(url):
 
     news.content = content
 
-    news.url = url  # 新闻页面对应的url
+    news.url = url  # news url
     f.write(news.topic + '\t' + news.content + '\n')
 
-#dfs算法遍历全站
+## DFS(depth first search) algorithm traverses the entire site
 def dfs(url):
     global count
     print(url)
 
-#下面的部分需要修改月份以更换爬取月份
-    pattern1 = '^http://stock.jrj.com.cn/2012/06\/[a-z0-9_\/\.]*$'  # 可以继续访问的url规则
-    pattern2 = '^http://stock.jrj.com.cn/2012/06\/[0-9]{14}\.shtml$'  # 解析新闻信息的url规则
-    pattern3 = '^http://stock.jrj.com.cn/xwk/201001/201001[0-9][0-9]\_[0-9]\.shtml$' # 测试
-
-    # 该url访问过，则直接返回
+## The following sections need to modify the month to replace the crawling month
+    pattern1 = '^http://stock.jrj.com.cn/2012/06\/[a-z0-9_\/\.]*$'  # Url rules that you can continue to access
+    pattern2 = '^http://stock.jrj.com.cn/2012/06\/[0-9]{14}\.shtml$'  # Url rules for extracting news information
+    
+    # If the url has been accessed, then returns directly
     if url in visited:  return
     print(url)
 
-    # 把该url添加进visited()
+    # Add the url to visited()
     visited.add(url)
     # print(visited)
-    # 设置停顿时间为1秒
+    # Set the pause time to 1 second, in order to reduce the risk of being targeted by the web-masters
     # time.sleep(1)
     try:
-        # 该url没有访问过的话，则继续解析操作
+        # If the url has not been accessed, then continue
         html = request.urlopen(url).read().decode('utf-8','ignore')
         # print(html)
         soup = BeautifulSoup(html, 'html.parser')
@@ -89,7 +87,7 @@ def dfs(url):
             getNews(url)
         # count += 1
 
-        # 提取该页面其中所有的url
+        # Extract all the urls in the page
         links = soup.findAll('a', href=re.compile(pattern1))
         for link in links:
             print(link['href'])
@@ -103,18 +101,18 @@ def dfs(url):
         print(e)
         return
 
-
 # print(count)
 # if count > 3: return
 
-visited = set()  # 存储访问过的url
+visited = set()  # Stores the url that has been accessed
 
 f = open('C:/news.txt', 'a+', encoding='utf-8')
 
-# 利用list对整个月份中所有的分页的进行遍历，此操作能够使得爬虫在遍历当前页面所有url的基础上，再遍历每个月份的所有分页内的所有新闻。
+## Use list to traverse all part of one month pages. 
+## This operation enables the crawler to traverse all news in all pages of each month based on traversing all the urls of the current page.
 urls=['http://stock.jrj.com.cn/xwk/201206/201206{}{}_{}.shtml'.format(str(i),str(j),str(k)) for i in range (0,4) for j in range(1,10) for k in range(1,6)]
 for url in urls:
-    if url == 'http://stock.jrj.com.cn/xwk/201206/20120631_5.shtml':        # 需要修改以更换月份
+    if url == 'http://stock.jrj.com.cn/xwk/201206/20120631_5.shtml':        # Need to change
         dfs(url)
         break
     dfs(url)
